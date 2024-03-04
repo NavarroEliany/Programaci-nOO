ecbd2c94aa1b455a3398ef6113a2a9132b13c742
NavarroEliany <elnavarrob@ufpso.edu.co>
2024-03-04 10:55:26 -0500

    getters y settters cobrecarga de constructor

---
+++ programacion/src/main/java/com/mycompany/programacion/Programacion.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.programacion;
+
+/**
+ *
+ * @author SCIS2PC23
+ */
+public class Programacion {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
